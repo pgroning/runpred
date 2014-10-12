3f6f5b2c0b57657366ccde5791231bb1c539e3e0
Per Gröningsson <per.groningsson@gmail.com>
2014-10-12 16:22:16 +0200

    added pace

--- runpred.py
+++ runpred.py
@@ -59,14 +59,19 @@ def main():
     print ("Running distance must be specified")
     print ("type 'runpred --help' for more information")
     sys.exit(2)
-  else: # Calculate speed in km/min
-    speed_m_min = float(distance)*1000/runtime_min
+  #else: # Calculate speed in km/min
+    #speed_m_min = float(distance)*1000/runtime_min
 
-  VO2max = VO2max_fun(speed_m_min,runtime_min)
-  print (VO2max)
+  # Calculate running pace
+  pace = runtime_min/float(distance) # min/km
+  print ("Pace = {0}:{1} min/km".format(int(pace),int(pace % 1 * 60)))
+  # Calculate VO2max
+  VO2max = VO2max_fun(distance,runtime_min)
+  print ("VO2max = {0}".format(VO2max))
 
-def VO2max_fun(speed_m_min, runtime_min):
+def VO2max_fun(distance, runtime_min):
   # VO2max according to Jack Daniels formula
+  speed_m_min = float(distance)*1000/runtime_min
   VO2 = -4.6+0.182258*speed_m_min+0.000104*speed_m_min**2
   percent_max = 0.8+0.1894393*exp(-0.012778*runtime_min) \
   + 0.2989558*exp(-0.1932605*runtime_min)
